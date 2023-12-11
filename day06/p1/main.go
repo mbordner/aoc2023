27c380be144068b692a1252d67f94b7fa473c4f4
Michael Bordner <mbordner@gmail.com>
2023-12-10 20:32:01 -0500

    day 6

---
+++ day06/p1/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mbordner/aoc2023/common/array/ints"
+	"github.com/mbordner/aoc2023/common/file"
+	"strings"
+)
+
+func wins(time, distance int64) (distances []int64) {
+	distances = make([]int64, 0, time)
+	for i := int64(0); i <= time; i++ {
+		d := i * (time - i)
+		if d > distance {
+			distances = append(distances, d)
+		}
+	}
+	return
+}
+
+func main() {
+	times, distances := getData("../data.txt")
+	margin := int64(1)
+	for i := range times {
+		ds := wins(times[i], distances[i])
+		margin *= int64(len(ds))
+	}
+	fmt.Println(margin)
+}
+
+func getData(path string) (times, distances []int64) {
+	lines, _ := file.GetLines(path)
+	times = ints.NumVals(lines[0][strings.Index(lines[0], ":")+1:])
+	distances = ints.NumVals(lines[1][strings.Index(lines[1], ":")+1:])
+	return
+}
